ce7d78a8c0507e634627235f80b33992efba9482
Takuro Wada <taxpon@gmail.com>
2021-05-16 11:51:42 +0900

    rebuild pages

--- camo-app/rollup.config.js
+++ camo-app/rollup.config.js
@@ -86,48 +86,18 @@ export default [
 		input: 'src/camo-pattern-js/index.ts',
 		output: {
 			sourcemap: !production,
-			format: 'iife',
-			name: 'app',
+			format: 'esm',
 			file: 'public/build/camo-pattern.js'
 		},
 		plugins: [
-			// svelte({
-			// 	preprocess: sveltePreprocess({sourceMap: !production}),
-			// 	compilerOptions: {
-			// 		// enable run-time checks when not in production
-			// 		dev: !production
-			// 	}
-			// }),
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			// css({output: 'bundle.css'}),
-
-			// If you have external dependencies installed from
-			// npm, you'll most likely need these plugins. In
-			// some cases you'll need additional configuration -
-			// consult the documentation for details:
-			// https://github.com/rollup/plugins/tree/master/packages/commonjs
-			// resolve({
-			// 	browser: true,
-			// 	dedupe: ['svelte']
-			// }),
-			commonjs(),
 			typescript({
 				sourceMap: !production,
-				inlineSources: !production
+				inlineSources: !production,
+				tsconfig: "./tsconfig.lib.json"
 			}),
-
-			// In dev mode, call `npm run start` once
-			// the bundle has been generated
-			// !production && serve(),
-
-			// Watch the `public` directory and refresh the
-			// browser on changes when not in production
-			// !production && livereload('public'),
-
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			// production && terser()
+			!production && serve(),
+			!production && livereload('public'),
+			production && terser()
 		],
 		watch: {
 			clearScreen: false
--- camo-app/src/camo-pattern-js/index.ts
+++ camo-app/src/camo-pattern-js/index.ts
@@ -27,9 +27,13 @@ export class CamoPatternM90P2Options implements CamoPatternOptions{
 }
 
 export class CamoPattern {
-    private _pattern: PatternType
-    private patterns: { [name: string]: BaseLogic }
+    private _pattern: PatternType = Pattern.M90P2;
+    private patterns: { [name: string]: BaseLogic } = {};
     private _mouseTrackEnabled = false;
+    private _width: number;
+    private _height: number;
+    private _colors: string[];
+    private readonly defaultM90P2Options = new CamoPatternM90P2Options(100, 2, false);
 
     // Constant
     private readonly ctx: CanvasRenderingContext2D
@@ -40,43 +44,69 @@ export class CamoPattern {
 
     constructor(private canvas: HTMLCanvasElement) {
         this.ctx = canvas.getContext("2d");
+        this._height = Number(canvas.getAttribute("width"));
+        this._width = Number(canvas.getAttribute("height"));
+        this._colors = [
+            "#6C7C52", // Light green
+            "#4A4F44", // Deep green
+            "#3B3845", // Dark Gray
+            "#B2B096", // Ivory
+        ];
     }
 
     set pattern(value: PatternType) {
-        this._pattern = value
+        this._pattern = value;
     }
 
     set mouseTrackEnabled(value: boolean) {
-        this._mouseTrackEnabled = value
+        this._mouseTrackEnabled = value;
     }
 
-    redraw(colors: string[], option: CamoPatternOptions) {
+    set width(value: number) {
+        this._width = value;
+    }
+
+    set height(value: number) {
+        this._height = value;
+    }
+
+    set colors(value: string[]) {
+        this._colors = value;
+    }
+
+    draw(width: number = undefined, height: number = undefined, colors: string[] = undefined, option: CamoPatternOptions = undefined) {
         if (!this.ctx) return;
-        let width = document.documentElement.clientWidth;
-        let height = document.documentElement.clientHeight;
-        this.clearCanvas(width, height, colors);
-        this.draw(width, height, colors, option)
-    }
 
-    draw(width: number, height: number, colors = undefined, option: CamoPatternOptions) {
-        const logic = this.getPatternLogic(this._pattern);
-        const _option = option ? option.asDict() : {};
-        logic.draw(width, height, colors, _option);
-    }
+        const _width = width || this._width;
+        const _height = height || this._height;
+        const _colors = colors || this._colors;
+        const _option = option || this.defaultM90P2Options;
 
-    clearCanvas(width, height, colors) {
-        this.canvas.setAttribute("width", width.toString());
-        this.canvas.setAttribute("height", height.toString());
-        // this.ctx.fillStyle = colors ?  colors.next().value : Color.getBaseColorFromPalette(color);
-        this.ctx.fillStyle = colors[0];
-        this.ctx.fillRect(0, 0, width, height);
+        this.clearCanvas(_width, _height, _colors);
+        this.drawPattern(_width, _height, _colors, _option);
     }
 
     handleMouseMove(e: MouseEvent) {
-        if (this.mouseTrackEnabled && this._pattern === Pattern.M90P2) {
+        if (this._mouseTrackEnabled && this._pattern === Pattern.M90P2) {
             const logic = this.getPatternLogic(this._pattern);
             logic.handleMouseMove(new Point(e.clientX, e.clientY))
         }
+    }
+
+    startAnimate() {
+        if (this._pattern !== Pattern.M90P2) {
+            console.error(`${this._pattern} doesn't support animation`);
+            return;
+        }
+        this.getPatternLogic(this._pattern).startAnimate();
+    }
+
+    stopAnimate() {
+        if (this._pattern !== Pattern.M90P2) {
+            console.error(`${this._pattern} doesn't support animation`);
+            return;
+        }
+        this.getPatternLogic(this._pattern).stopAnimate();
     }
 
     download() {
@@ -89,6 +119,20 @@ export class CamoPattern {
                 .replace("image/png", "image/octet-stream"));
         link.click();
         document.body.removeChild(link);
+    }
+
+    private drawPattern(width: number, height: number, colors = undefined, option: CamoPatternOptions) {
+        const logic = this.getPatternLogic(this._pattern);
+        const _option = option ? option.asDict() : {};
+        logic.draw(width, height, colors, _option);
+    }
+
+    private clearCanvas(width, height, colors) {
+        this.canvas.setAttribute("width", width.toString());
+        this.canvas.setAttribute("height", height.toString());
+        // this.ctx.fillStyle = colors ?  colors.next().value : Color.getBaseColorFromPalette(color);
+        this.ctx.fillStyle = colors[0];
+        this.ctx.fillRect(0, 0, width, height);
     }
 
     private getPatternLogic(pattern: string) {
